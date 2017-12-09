da0a95764bcde16b4d4f5f6261474d4f7c03b774
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2017-12-09 01:37:10 -0800

    Use -t in docker run to show git clone status

--- compile-kernel.sh
+++ compile-kernel.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -xe
 docker build -t jetson-tx2-linux docker/
-docker run --rm -v "$(readlink -f "$(dirname "$0")")":/jetson-tx2-linux jetson-tx2-linux
+docker run --rm -t -v "$(readlink -f "$(dirname "$0")")":/jetson-tx2-linux jetson-tx2-linux
