acc47065bc0e3625abb58736f0eb04bad0a549bf
Renato Freitas <freitas-renato@outlook.com>
2020-02-05 11:51:03 -0300

    Update to latest download sources

--- download-sources.sh
+++ download-sources.sh
@@ -3,8 +3,9 @@ set -xe
 cd "$(dirname "$0")"
 
 # Required for flashing and kernel building
-wget -c http://developer2.download.nvidia.com/embedded/L4T/r28_Release_v2.0/BSP/Tegra186_Linux_R28.2.0_aarch64.tbz2
-tar xf Tegra186_Linux_R28.2.0_aarch64.tbz2
+wget -c https://developer.nvidia.com/embedded/dlc/r32-3-1_Release_v1.0/t186ref_release_aarch64/Tegra186_Linux_R32.3.1_aarch64.tbz2
+tar xvf Tegra186_Linux_R32.3.1_aarch64.tbz2
 
 # Required for flashing
-wget -c -O rootfs.tbz2 https://developer.nvidia.com/embedded/dlc/l4t-sample-root-filesystem-28-2
+wget -c -O rootfs.tbz2 https://developer.nvidia.com/embedded/dlc/r32-3-1_Release_v1.0/t186ref_release_aarch64/Tegra_Linux_Sample-Root-Filesystem_R32.3.1_aarch64.tbz2
+
